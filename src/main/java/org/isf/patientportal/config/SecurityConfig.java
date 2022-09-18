48ca6f9d4ad78406cc8313c60f3cf71cbd3aacf5
Antonio Verni <me.verni@gmail.com>
2022-09-18 20:35:56 +0200

    chore: indent

--- src/main/java/org/isf/patientportal/config/SecurityConfig.java
+++ src/main/java/org/isf/patientportal/config/SecurityConfig.java
@@ -1,4 +1,3 @@
-
 package org.isf.patientportal.config;
 
 import java.util.ArrayList;
@@ -178,20 +177,22 @@ public class SecurityConfig {
     }
 
     @Bean
-    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier, ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier, EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties, WebEndpointProperties webEndpointProperties, Environment environment) {
-            List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
-            Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
-            allEndpoints.addAll(webEndpoints);
-            allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
-            allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
-            String basePath = webEndpointProperties.getBasePath();
-            EndpointMapping endpointMapping = new EndpointMapping(basePath);
-            boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment, basePath);
-            return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes, corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath), shouldRegisterLinksMapping, null);
-        }
+    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(
+        WebEndpointsSupplier webEndpointsSupplier, ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier, 
+        EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties, WebEndpointProperties webEndpointProperties, Environment environment) {
+        List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
+        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+        allEndpoints.addAll(webEndpoints);
+        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+        String basePath = webEndpointProperties.getBasePath();
+        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment, basePath);
+        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes, corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath), shouldRegisterLinksMapping, null);
+    }
 
 
     private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment, String basePath) {
-            return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
-        }
+        return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
+    }
 }
