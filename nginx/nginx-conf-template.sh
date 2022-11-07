2b895bfd9b1e48024461f1a219301a451419eb1c
Antonio Verni <me.verni@gmail.com>
2022-11-07 12:01:34 +0100

    first add

---
+++ nginx/nginx-conf-template.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+[ -f /etc/nginx/nginx.conf.template ] && {
+    echo "Generating nginx.conf from template"
+    envsubst '${WORKER_PROCESSES} ${WORKER_CONNECTIONS} ${WORKER_NAME}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+}
