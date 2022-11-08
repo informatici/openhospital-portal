7ffc39718d407ffa80cac1879159b02480fb6571
Antonio Verni <me.verni@gmail.com>
2022-11-08 12:01:53 +0100

    feat: collegamento ui loadbalancer

--- patient-portal-ui/docker/nginx-conf-template.sh
+++ patient-portal-ui/docker/nginx-conf-template.sh
@@ -2,5 +2,6 @@
 
 [ -f /etc/nginx/nginx.conf.template ] && {
     echo "Generating nginx.conf from template"
+    envsubst '${WORKER_PROCESSES} ${WORKER_CONNECTIONS} ${WORKER_NAME}' < /etc/nginx/nginx.conf.template
     envsubst '${WORKER_PROCESSES} ${WORKER_CONNECTIONS} ${WORKER_NAME}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 }
