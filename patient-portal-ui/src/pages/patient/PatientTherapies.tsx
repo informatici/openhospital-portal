e36339ce35b6393eed94bccd19bf6028add7a108
Serdf3rISF <128729231+Serdf3rISF@users.noreply.github.com>
2023-11-21 15:42:17 +0100

    PP-39 UI fix graphics and themes (#27)
    * DB
    
    * solve navbar in Agenda
    
    * re-solve NavBar in PatientAgenda
    
    * resolve NavBar in PatientAgenda
    
    * Patient Agenda OK to at
    
    * clean code
    
    * clean code
    
    * outside profile should not interact with underneath buttons
    
    * part table
    
    * table, color text and backgorund nav
    
    * button rounded
    
    ---------
    
    Co-authored-by: Serdf3r <serdf3r@gmail.com>
    Co-authored-by: Alessandro Domanico <alessandro.domanico@yahoo.it>

--- patient-portal-ui/src/pages/patient/PatientTherapies.tsx
+++ patient-portal-ui/src/pages/patient/PatientTherapies.tsx
@@ -81,8 +81,8 @@ const PatientTherapies = () => {
             // justifyContent="flex-end" # DO NOT USE THIS WITH 'scroll'
           }}
         >
-          <ButtonGroup sx={{ mt: 1, mb: 1, overflowX: "scroll", }} variant="outlined" aria-label="outlined button group">
-            <Button key="all" color="primary" onClick={() => setType(null)}>All</Button>
+          <ButtonGroup disableElevation className="button_group_f" sx={{ mt: 1, mb: 1, overflowX: " scroll", }} variant="outlined" aria-label="outlined button group">
+            <Button variant="contained" key="all" color="primary" onClick={() => setType(null)}>All</Button>
             {btFilters.map((bt_el) => (
               <Button key={bt_el} color="primary" onClick={() => setType(bt_el)}>{bt_el}</Button>
             ))}
@@ -136,7 +136,7 @@ const PatientTherapies = () => {
           columns={columns}
         />
       </div>
-    </Container>
+    </Container >
   );
 
 };
