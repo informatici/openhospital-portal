cbc3e25fb11ba861304c9ba571a8684f560c5776
Tiziano <tiziano.scrimieri@informaticisenzafrontiere.org>
2024-04-12 14:40:21 +0200

    only smart everywhere

--- patient-portal-ui/src/App.js
+++ patient-portal-ui/src/App.js
@@ -42,7 +42,7 @@ import AdministrationManageMisure from "./pages/administration/AdministrationMan
 import AdministrationResetPassword from "./pages/administration/AdministrationResetPassword";
 import ManageUsersAdministration from "./pages/administration/ManageUsersAdministration";
 import AdministrationOtherConfiguration from "./pages/administration/AdministrationOtherConfiguration";
-
+import useMediaQuery from '@mui/material/useMediaQuery';
 import { red } from '@mui/material/colors';
 
 import { ThemeProvider, createTheme } from '@mui/material/styles';
@@ -53,95 +53,113 @@ export const ThemeContext = React.createContext();
 export const UserContext = React.createContext();
 function App(props) {
 
-
+  
   const [auth, setAuth] = useState(false);
   const [profile, setProfile] = useState("aaaaa");
   const location = useLocation();
   const [theme, setTheme] = useState(theme_1);
+  const matches = useMediaQuery(theme.breakpoints.up('sm'));
 
   const value = { theme, setTheme };
 
+  if (matches) {
+    return (
+      <>
+        <Context.Provider value={{ theme, setTheme }} >
+          <ThemeProvider theme={theme}>
+          <CssBaseline />
+            <p className='only_smart'>Only Smart Phone</p>
+          </ThemeProvider>
+        </Context.Provider>
+      </>
+    );
+  } else {
 
-  return (
-    <Context.Provider value={{ theme, setTheme }} >
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Routes>
-          <Route path="/logout" element={<Login setAuth={setAuth} setProfile={setProfile} />} />
-          <Route path="/login" element={<Login setAuth={setAuth} setProfile={setProfile} />} />
-          <Route path="/signup" element={<Signup setAuth={setAuth} />} />
-          <Route path="/resetPassword" element={<ResetPassword setAuth={setAuth} />} />
+    return (
 
-          <Route path="/PatientHome" element={<PatientHome setAuth={setAuth} />} />
-          <Route path="/PatientChooseMeasurements" element={<PatientChooseMeasurements setAuth={setAuth} />} />
-          <Route path="/PatientInsertMeasurements" element={<PatientInsertMeasurements setAuth={setAuth} setType={location.state} />} />
-          <Route path="/PatientMeasurements" element={<PatientMeasurements setAuth={setAuth} />} />
-          <Route path="/PatientAgenda" element={<PatientAgenda setAuth={setAuth} />} />
-          <Route path="/PatientAdsDeadlines" element={<PatientAdsDeadlines setAuth={setAuth} />} />
-          <Route path="/PatientExams" element={<PatientExams setAuth={setAuth} />} />
-          <Route path="/PatientExamDetails" element={<PatientExamDetails setAuth={setAuth} setType={location.state} />} />
-          <Route path="/PatientHospitalizations" element={<PatientHospitalizations setAuth={setAuth} />} />
-          <Route path="/PatientHospitalizationDetails" element={<PatientHospitalizationDetails setAuth={setAuth} setType={location.state} />} />
-          <Route path="/PatientPayments" element={<PatientPayments setAuth={setAuth} />} />
-          <Route path="/PatientPaymentDetails" element={<PatientPaymentDetails setAuth={setAuth} setType={location.state} />} />
-          <Route path="/PatientTherapies" element={<PatientTherapies setAuth={setAuth} />} />
-          <Route path="/PatientTherapieDetails" element={<PatientTherapieDetails setAuth={setAuth} setType={location.state} />} />
-          <Route path="/PatientVaccinations" element={<PatientVaccinations setAuth={setAuth} />} />
-          <Route path="/PatientVaccinationDetails" element={<PatientVaccinationDetails setAuth={setAuth} setType={location.state} />} />
-          <Route path="/PatientVisits" element={<PatientVisits setAuth={setAuth} />} />
-          <Route path="/PatientVisitDetails" element={<PatientVisitDetails setAuth={setAuth} setType={location.state} />} />
+      <Context.Provider value={{ theme, setTheme }} >
 
-          <Route path="/DoctorHome" element={<DoctorHome setAuth={setAuth} />} />
-          <Route path="/DoctorAgenda" element={<DoctorAgenda setAuth={setAuth} />} />
-          <Route path="/DoctorjReqAuth" element={<DoctorjReqAuth setAuth={setAuth} setType={location.state} />} />
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
 
-          <Route path="/AdministrationHome" element={<AdministrationHome setAuth={setAuth} />} />
-          <Route path="/AdministrationResetPassword" element={<AdministrationResetPassword setAuth={setAuth} />} />
-          <Route path="/AdministrationManageMisure" element={<AdministrationManageMisure setAuth={setAuth} />} />
-          <Route path="/ManageUsersAdministration" element={<ManageUsersAdministration setAuth={setAuth} />} />
-          <Route path="/AdministrationOtherConfiguration" element={<AdministrationOtherConfiguration setAuth={setAuth} />} />
+          <Routes>
+            <Route path="/logout" element={<Login setAuth={setAuth} setProfile={setProfile} />} />
+            <Route path="/login" element={<Login setAuth={setAuth} setProfile={setProfile} />} />
+            <Route path="/signup" element={<Signup setAuth={setAuth} />} />
+            <Route path="/resetPassword" element={<ResetPassword setAuth={setAuth} />} />
 
-          <Route path="/SelectLanguage" element={<SelectLanguage setAuth={setAuth} />} />
-          <Route
-            path="/"
-            // element={
-            //   auth ? (
-            //     profile == "Admin" ? (
-            //       <AdminHome setAuth={setAuth} />
-            //     ) : profile == "Administration" ? (
-            //       <AdministrationHome setAuth={setAuth} />
-            //     ) : profile == "Doctor" ? (
-            //       <DoctorHome setAuth={setAuth} />
-            //     ) : profile == "Patient" ? (
-            //       <PatientHome theme={theme_2} setAuth={setAuth} />
-            //     ) : profile == "Unauthorized" ? (
-            //       <Navigate to="/login" state={{ from: location }} replace />
-            //     ) :
-            //       <Navigate to="/login" state={{ from: location }} replace />
-            //   ) : (
-            //     <Navigate to="/login" state={{ from: location }} replace />
-            //   )
-            // }
-            element={
-              auth ? (
-                profile == "Admin" ? (
-                  <AdminHome setAuth={setAuth} />
-                ) : profile == "Administration" ? (
-                  <AdministrationHome setAuth={setAuth} />
-                ) : profile == "Doctor" ? (
-                  <DoctorHome setAuth={setAuth} />
-                ) : profile == "Patient" ? (
-                  <PatientHome theme={theme_2} setAuth={setAuth} />
-                ) :
-                  <PatientHome setAuth={setAuth} />
-              ) : (
-                <Navigate to="/login" state={{ from: location }} replace />
-              )
-            }
-          />
-        </Routes>
-      </ThemeProvider>
-    </Context.Provider>
-  );
+            <Route path="/PatientHome" element={<PatientHome setAuth={setAuth} />} />
+            <Route path="/PatientChooseMeasurements" element={<PatientChooseMeasurements setAuth={setAuth} />} />
+            <Route path="/PatientInsertMeasurements" element={<PatientInsertMeasurements setAuth={setAuth} setType={location.state} />} />
+            <Route path="/PatientMeasurements" element={<PatientMeasurements setAuth={setAuth} />} />
+            <Route path="/PatientAgenda" element={<PatientAgenda setAuth={setAuth} />} />
+            <Route path="/PatientAdsDeadlines" element={<PatientAdsDeadlines setAuth={setAuth} />} />
+            <Route path="/PatientExams" element={<PatientExams setAuth={setAuth} />} />
+            <Route path="/PatientExamDetails" element={<PatientExamDetails setAuth={setAuth} setType={location.state} />} />
+            <Route path="/PatientHospitalizations" element={<PatientHospitalizations setAuth={setAuth} />} />
+            <Route path="/PatientHospitalizationDetails" element={<PatientHospitalizationDetails setAuth={setAuth} setType={location.state} />} />
+            <Route path="/PatientPayments" element={<PatientPayments setAuth={setAuth} />} />
+            <Route path="/PatientPaymentDetails" element={<PatientPaymentDetails setAuth={setAuth} setType={location.state} />} />
+            <Route path="/PatientTherapies" element={<PatientTherapies setAuth={setAuth} />} />
+            <Route path="/PatientTherapieDetails" element={<PatientTherapieDetails setAuth={setAuth} setType={location.state} />} />
+            <Route path="/PatientVaccinations" element={<PatientVaccinations setAuth={setAuth} />} />
+            <Route path="/PatientVaccinationDetails" element={<PatientVaccinationDetails setAuth={setAuth} setType={location.state} />} />
+            <Route path="/PatientVisits" element={<PatientVisits setAuth={setAuth} />} />
+            <Route path="/PatientVisitDetails" element={<PatientVisitDetails setAuth={setAuth} setType={location.state} />} />
+
+            <Route path="/DoctorHome" element={<DoctorHome setAuth={setAuth} />} />
+            <Route path="/DoctorAgenda" element={<DoctorAgenda setAuth={setAuth} />} />
+            <Route path="/DoctorjReqAuth" element={<DoctorjReqAuth setAuth={setAuth} setType={location.state} />} />
+
+            <Route path="/AdministrationHome" element={<AdministrationHome setAuth={setAuth} />} />
+            <Route path="/AdministrationResetPassword" element={<AdministrationResetPassword setAuth={setAuth} />} />
+            <Route path="/AdministrationManageMisure" element={<AdministrationManageMisure setAuth={setAuth} />} />
+            <Route path="/ManageUsersAdministration" element={<ManageUsersAdministration setAuth={setAuth} />} />
+            <Route path="/AdministrationOtherConfiguration" element={<AdministrationOtherConfiguration setAuth={setAuth} />} />
+
+            <Route path="/SelectLanguage" element={<SelectLanguage setAuth={setAuth} />} />
+            <Route
+              path="/"
+              // element={
+              //   auth ? (
+              //     profile == "Admin" ? (
+              //       <AdminHome setAuth={setAuth} />
+              //     ) : profile == "Administration" ? (
+              //       <AdministrationHome setAuth={setAuth} />
+              //     ) : profile == "Doctor" ? (
+              //       <DoctorHome setAuth={setAuth} />
+              //     ) : profile == "Patient" ? (
+              //       <PatientHome theme={theme_2} setAuth={setAuth} />
+              //     ) : profile == "Unauthorized" ? (
+              //       <Navigate to="/login" state={{ from: location }} replace />
+              //     ) :
+              //       <Navigate to="/login" state={{ from: location }} replace />
+              //   ) : (
+              //     <Navigate to="/login" state={{ from: location }} replace />
+              //   )
+              // }
+              element={
+                auth ? (
+                  profile == "Admin" ? (
+
+                    <AdminHome setAuth={setAuth} />
+                  ) : profile == "Administration" ? (
+                    <AdministrationHome setAuth={setAuth} />
+                  ) : profile == "Doctor" ? (
+                    <DoctorHome setAuth={setAuth} />
+                  ) : profile == "Patient" ? (
+                    <PatientHome theme={theme_2} setAuth={setAuth} />
+                  ) :
+                    <PatientHome setAuth={setAuth} />
+                ) : (
+                  <Navigate to="/login" state={{ from: location }} replace />
+                )
+              }
+            />
+          </Routes>
+        </ThemeProvider>
+      </Context.Provider>
+    );
+  }
 }
 export default App;
