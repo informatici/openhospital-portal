92fa190321dc760413028ea2a0029d48bf4ef3d9
mwithi <alessandro.domanico@yahoo.it>
2024-06-18 22:04:00 +0200

    Add common files and initialize core

--- connector/src/main/java/org/isf/patientportal/connector/ConnectorApplication.java
+++ connector/src/main/java/org/isf/patientportal/connector/ConnectorApplication.java
@@ -21,15 +21,14 @@
  */
 package org.isf.patientportal.connector;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
+import org.isf.generaldata.GeneralData;
 import org.isf.patientportal.connector.common.SyncReport;
 import org.isf.patientportal.connector.config.ConnectorConfiguration;
 import org.isf.patientportal.connector.sync.common.SyncResult;
@@ -44,51 +43,51 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
-@ImportResource({"classpath*:**/applicationContext.xml"})
+@ImportResource({ "classpath*:**/applicationContext.xml" })
 @SpringBootApplication
 public class ConnectorApplication implements CommandLineRunner {
 
-    private static final Logger logger = LoggerFactory.getLogger(ConnectorApplication.class);
-    
-    @Autowired
-    private PatientSource patientSource;
+	private static final Logger logger = LoggerFactory.getLogger(ConnectorApplication.class);
 
+	@Autowired
+	private PatientSource patientSource;
 
-    public static void main(String[] args) {
-        SpringApplication.run(ConnectorApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectorApplication.class, args);
+	}
 
-    @Bean
-    protected PatientSyncer patientSyncer() {
-        return new PatientSyncer();
-    }
+	@Bean
+	protected PatientSyncer patientSyncer() {
+		return new PatientSyncer();
+	}
 
-    @Override
-    public void run(String... args) throws Exception {
-        logger.info("Starting oh connector using config ");
-        logger.info("Api url " + ConnectorConfiguration.API_URL);
-        logger.info("Using " + ConnectorConfiguration.PARALLEL + " threads");
+	@Override
+	public void run(String... args) throws Exception {
+		GeneralData.initialize();
+		logger.info("Starting oh connector using config ");
+		logger.info("Api url " + ConnectorConfiguration.API_URL);
+		logger.info("Using " + ConnectorConfiguration.PARALLEL + " threads");
 
-        ExecutorService executor = Executors.newFixedThreadPool(ConnectorConfiguration.PARALLEL);
-        List<Integer> patientIds = patientSource.getPatientIds();
-        logger.info("Found " + patientIds.size() + " patients to sync");
-        List<Callable<SyncResult>> patientsToSync = new ArrayList<Callable<SyncResult>>();
-        for (Integer patientId : patientIds) {
-            PatientSyncer patientSyncer = new PatientSyncer(patientId);
-            patientSyncer.setPatientManager(patientSource.getPatientManager());
-            patientsToSync.add(patientSyncer);
-        }
-        List<Future<SyncResult>> results = executor.invokeAll(patientsToSync);
-        executor.shutdown();
-        
-        SyncReport report = new SyncReport();
-        for (Future<SyncResult> future : results) {
-            SyncResult result = future.get();
-            report.addResult(result);
-        }
+		ExecutorService executor = Executors.newFixedThreadPool(ConnectorConfiguration.PARALLEL);
+		List<Integer> patientIds = patientSource.getPatientIds();
+		logger.info("Found " + patientIds.size() + " patients to sync");
+		List<Callable<SyncResult>> patientsToSync = new ArrayList<Callable<SyncResult>>();
+		for (Integer patientId : patientIds) {
+			PatientSyncer patientSyncer = new PatientSyncer(patientId);
+			patientSyncer.setPatientManager(patientSource.getPatientManager());
+			patientsToSync.add(patientSyncer);
+		}
+		List<Future<SyncResult>> results = executor.invokeAll(patientsToSync);
+		executor.shutdown();
 
-        logger.info("Sync report: " + report);
-        
-    }
+		SyncReport report = new SyncReport();
+		for (Future<SyncResult> future : results) {
+			SyncResult result = future.get();
+			report.addResult(result);
+		}
+
+		logger.info("Sync report: " + report);
+
+	}
 
 }
